5b2622aca8acd7176f39a742db02d09c5bac8300
JackCurtis <jack.v.curtis@gmail.com>
2015-02-26 15:00:30 -0500

    toggle spending v. partisanship

--- app/assets/javascripts/views/map_view.js
+++ app/assets/javascripts/views/map_view.js
@@ -1,22 +1,44 @@
 var MapView = Backbone.View.extend({
   el: ".js-map",
 
+  events: {
+    "change #color-toggle" : "colorMap"
+  },
+
   initialize: function(){
     this.listenTo(this.collection, 'reset', this.colorMap)
   },
 
   colorMap: function(){
-    var baseColor = "#0000ff";
+    if(this.$("#color-toggle").is(":checked")){
+      this.colorPartisanship();
+    }
+    else {
+      this.colorSpending();
+    }
+  },
+
+  colorPartisanship: function(){
+    var stateTotals = this.collection.sumByState();
+    for(var i = 0; i < stateTotals.length; i++){
+      var state = stateTotals[i][0];
+      var repSum = stateTotals[i][2];
+      var demSum = stateTotals[i][3];
+      var partisanSum = repSum + demSum;
+      var repWeight = parseInt((repSum / partisanSum) * 200);
+      var demWeight = parseInt((demSum / partisanSum) * 200);
+      this.$("#"+state).css("fill", "rgb("+ (200 - demWeight) +", 0, "+ (200 - repWeight) +")");
+    }
+  },
+
+  colorSpending: function(){
     var stateTotals = this.collection.sumByState();
     var max = this.collection.stateMax();
     for(var i = 0; i < stateTotals.length; i++){
       var state = stateTotals[i][0];
       var sum = stateTotals[i][1];
-      var repSum = stateTotals[i][2];
-      var demSum = stateTotals[i][3];
-      var repWeight = parseInt((repSum / max) * 150);
-      var demWeight = parseInt((demSum / max) * 150);
-      this.$("#"+state).css("fill", "rgb("+ (200 - demWeight) +", 0, "+ (200 - repWeight) +")");
+      var colorWeight = parseInt((sum / max) * 200);
+      this.$("#"+state).css("fill", "rgb(0, "+ (200 - colorWeight) +", 0)");
     }
   }
 });
