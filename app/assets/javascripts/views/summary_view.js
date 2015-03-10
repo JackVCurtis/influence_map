f931f4494c4c257f4b74b2e0f1267adb526a7c91
JackCurtis <jack.v.curtis@gmail.com>
2015-03-10 13:18:42 -0400

    add currency formatting

--- app/assets/javascripts/views/summary_view.js
+++ app/assets/javascripts/views/summary_view.js
@@ -27,9 +27,11 @@ var SummaryView = Backbone.View.extend({
   },
 
   getStats: function(){
-    $(".total").text(" " + this.collection.sum("amount"));
+    var total = this.collection.sum("amount");
+    var stateMax = this.collection.stateMax();
+    $(".total").text(" " + this.formatCurrency(total));
     $(".top-state").text(" " + this.collection.topState());
-    $(".top-state-total").text(" " + this.collection.stateMax());
+    $(".top-state-total").text(" " + this.formatCurrency(stateMax));
     $(".top-contributors").html(this.getTopContributors());
   },
 
@@ -38,10 +40,24 @@ var SummaryView = Backbone.View.extend({
     var html = "<tr><th>Contributor Name</th><th>Total</th></tr>";
     for(var i = 0; i < contributorTotals.length; i++){
       var name = "<td>" + contributorTotals[i].name + "</td>";
-      var total = "<td>" + contributorTotals[i].total + "</td>";
+      var total = "<td>" + this.formatCurrency(contributorTotals[i].total) + "</td>";
       html +=  "<tr>" + name + total + "</tr>";
     }
     return html;
   },
 
+  formatCurrency: function(num){
+    var numString = num.toString();
+    var currencyString = "";
+    for(var i = 0; i < numString.length; i++){
+      var digit = numString[numString.length - (i + 1)];
+      if(i % 3 === 0 && i !== 0){
+        currencyString = digit + "," + currencyString;
+      }
+      else {
+        currencyString = digit + currencyString;
+      }
+    }
+    return "$ " + currencyString;
+  }
 })
